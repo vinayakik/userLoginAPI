9fb2b431fe1418b81d1b88daada097dd9aeefdc6
vinayakik <vinayakkadlennavar@gmail.com>
2022-11-23 15:08:28 +0530

    changed file name to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const db=require('./db')
 
-const port=5000;
+const port= process.env.PORT ||5000;
 
 const cors=require('cors');  //cors->cross origin resource sharing. It helps in interacting with different origins. 
 app.use(cors())
@@ -11,5 +11,5 @@ const AuthController=require('./controller/AuthController');
 app.use('/api/auth',AuthController);
 
 app.listen(port,()=>{
-    console.log('listening on port 5000')
+    console.log('listening on port '+ port)
 })
